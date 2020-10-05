ace4d08a37bcb0055c9098f2e4f59af54d621083
Simon Oelerich <simon.oelerich@gmasil.de>
2020-10-06 00:06:12 +0200

    fix sonar findings

--- src/main/java/de/gmasil/mhw/ctceditor/ui/api/RepaintTreeCallback.java
+++ src/main/java/de/gmasil/mhw/ctceditor/ui/api/RepaintTreeCallback.java
@@ -1,6 +1,8 @@
 package de.gmasil.mhw.ctceditor.ui.api;
 
+import java.io.Serializable;
+
 @FunctionalInterface
-public interface RepaintTreeCallback {
+public interface RepaintTreeCallback extends Serializable {
 	public void repaintTree();
 }
