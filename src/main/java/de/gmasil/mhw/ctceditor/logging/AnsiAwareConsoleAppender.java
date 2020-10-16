65bbb045f016d382ebe45d92d330c36ce479f889
Simon Oelerich <simon.oelerich@gmasil.de>
2020-10-16 18:28:29 +0200

    add GNU GPL v3 license

--- src/main/java/de/gmasil/mhw/ctceditor/logging/AnsiAwareConsoleAppender.java
+++ src/main/java/de/gmasil/mhw/ctceditor/logging/AnsiAwareConsoleAppender.java
@@ -1,3 +1,22 @@
+/**
+ * MHW-CTC-Editor
+ * Copyright © 2020 Simon Oelerich (simon.oelerich@gmasil.de)
+ *
+ * This file is part of MHW-CTC-Editor.
+ *
+ * MHW-CTC-Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MHW-CTC-Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MHW-CTC-Editor. If not, see <https://www.gnu.org/licenses/>.
+ */
 package de.gmasil.mhw.ctceditor.logging;
 
 import org.fusesource.jansi.AnsiConsole;
